afa8c538fdf21341058b94ecaf39c49cbe9b8f92
gezhonglunta <right.hua.zhang@gmail.com>
2012-11-04 17:54:57 +0800

    pass word function

--- src/org/geometerplus/fbreader/fbreader/PassWordInput.java
+++ src/org/geometerplus/fbreader/fbreader/PassWordInput.java
@@ -1,11 +1,15 @@
 package org.geometerplus.fbreader.fbreader;
 
 import org.LXZKimage.R;
+import org.geometerplus.zlibrary.core.options.ZLStringOption;
+import org.geometerplus.zlibrary.core.resources.ZLResource;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PassWordInput extends Dialog implements
 		android.view.View.OnClickListener {
@@ -14,15 +18,39 @@ public class PassWordInput extends Dialog implements
 		super(context);
 	}
 
+	private EditText editText;
+	private Button btnOk;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pass_word_input);
+		final ZLResource resource = ZLResource.resource("messageBoxStr");
+		setTitle(resource.getResource("passwordTitle").getValue());
+		editText = (EditText) findViewById(R.id.passInput_Input);
+		btnOk = (Button) findViewById(R.id.passInput_Ok);
+		btnOk.setText(resource.getResource("passwordOk").getValue());
+		btnOk.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-
+		switch (v.getId()) {
+		case R.id.passInput_Ok:
+			passWord.setValue(editText.getText().toString());
+		default:
+			dismiss();
+		}
 	}
 
+	private static final ZLStringOption passWord = new ZLStringOption("Verify",
+			"verifyCode", "");
+
+	public static boolean hasPassWord() {
+		if ("123456".equals(passWord.getValue())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
